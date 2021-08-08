ef53f6e6fecadd5ad05216cf5226b175b28c395a
InjiChoi <chldlswl1212@gmail.com>
2021-08-08 23:59:35 +0900

    Chapter05-1. 꼭 필요한 자료구조 기초

---
+++ Chapter05. DFS,BFS/5-3_recursive_function.py
@@ -0,0 +1,8 @@
+# 재귀 함수 예제
+def recursive_function():
+    print("재귀 함수 호출")
+    recursive_function()
+
+
+recursive_function()
+# RecursionError: maximum recursion depth exceeded while pickling an object
